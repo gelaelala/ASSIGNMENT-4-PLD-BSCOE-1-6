a2d2d41567478b4df45aec17d54c8e05d8c1cf75
unknown <angelaescariocorpuz@gmail.com>
2021-11-08 13:19:51 +0800

    First program under assignment number 4

--- program 1.py
+++ program 1.py
@@ -1,7 +1,12 @@
-def getNameAgeAddress ():
+def getNameAgeAddress():
     Name = input("Enter name: ")
     Age = int(input("Enter age: "))
-    Address = input ("Enter address")
+    Address = input("Enter address: ")
     return Name, Age, Address
 
-def display (name_, age_, address_):
+def display(name_, age_, address_):
+    print (f'Hi, my name is {name_}. I am {age_} years old and I live in {address_}.')
+
+name, age, address = getNameAgeAddress() 
+
+display(name, age, address)
