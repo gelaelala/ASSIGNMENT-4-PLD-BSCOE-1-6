d9d6ba9dae965d6548dcdeeecf0b7a515ecccf4f
unknown <angelaescariocorpuz@gmail.com>
2021-11-08 13:11:55 +0800

    First program under assignment number 4

---
+++ program 1.py
@@ -0,0 +1,7 @@
+def getNameAgeAddress ():
+    Name = input("Enter name: ")
+    Age = int(input("Enter age: "))
+    Address = input ("Enter address")
+    return Name, Age, Address
+
+def display (name_, age_, address_):
