d661ce9e9549e821c3fea2f5c2476c592032fc7d
unknown <angelaescariocorpuz@gmail.com>
2021-11-08 14:05:06 +0800

    Third program under assignment number 4

--- program 3.py
+++ program 3.py
@@ -1,4 +1,19 @@
 def getMoneyPriceofapple():
     Money = int(input("Enter the amount of money: "))
     PriceofApple = int(input("Enter the price of the apple: "))
-    return Money, PriceofApple
+    numberofapples = int(Money/PriceofApple)
+    return Money, PriceofApple, numberofapples
+
+def totalandchange(numberofapples_, money_, priceofapple_):
+    total = int(numberofapples_*priceofapple_)
+    change = int(money_ - total)
+    return change
+
+def display(Numberofapples, total):
+    print (f'You can buy {Numberofapples} apples and your change is {total} pesos.')
+
+money, priceofapple, numberofapples = getMoneyPriceofapple()
+
+totalandchange_ = totalandchange(numberofapples, money, priceofapple)
+
+display(numberofapples, totalandchange_)
